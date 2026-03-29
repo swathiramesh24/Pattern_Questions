55f4d45211d75fe4a1f07e45685aed5d4a801b03
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2026-03-29 18:49:35 +0530

    Create Pattern 1.java

---
+++ Pattern 1.java
@@ -0,0 +1,27 @@
+/*
+  *****
+  *****
+  *****
+  *****
+  *****
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=0;i<5;i++)
+		{
+		    for(int j=0;j<5;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+
+	}
+}
